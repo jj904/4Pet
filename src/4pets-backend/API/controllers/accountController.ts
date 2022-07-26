4397b60b0a9cec713f3917eb9eb6389a446e482e
Alan Nguyen <nguyenalan2000@gmail.com>
2022-07-25 18:44:31 -0700

    added acct methods

---
+++ src/4pets-backend/API/controllers/accountController.ts
@@ -0,0 +1,88 @@
+import admin from 'firebase-admin';
+import httpStatus, { NOT_FOUND } from 'http-status';
+import { db } from '../db';
+
+export const getAccounts = async(req: any, res: any, next: any) => {
+    console.log("Called getAccounts method");
+
+    try {
+        db.collection('Users').get().then((snapshot) => {
+            snapshot.docs.map((doc => doc.data));
+            res.status(httpStatus.OK).json(snapshot.docs);
+        })
+    } catch (err) {
+        console.log("Could not get accounts");
+        next(err);
+    }
+}
+export const getAccount = async(req: any, res: any, next: any) => {
+    console.log("Called getAccount method");
+
+    const { accountId } = req.params;
+
+    try {
+        const accRef = db.collection('Users').doc(accountId);
+        let doc = await accRef.get();
+        if (!doc.exists) {
+            console.log("No document found, creating...");
+            admin.auth().getUserByEmail(accountId).then(async (user) => {
+                    await accRef.set({email: accountId});
+                    doc = await accRef.get();
+                })
+                .catch((er) => {
+                    console.log("User does not exist");
+                    res.status(NOT_FOUND).send('User does not exist');
+                });
+             
+        } else
+            res.status(httpStatus.OK).json(doc.data());
+    } catch (err) {
+        console.log("Could not get account");
+        next(err);
+    }
+}
+
+export const createAccount = async(req: any, res: any, next: any) => {
+    console.log("Called createAccount method");
+    
+    try {
+        const {name, password, email, phone, creditCard, payment} = req.body;
+        const toAdd = {
+            name,
+            password,
+            email,
+            phone
+        }
+        const ret = await db.collection('Users').doc(email).set(toAdd, {merge:true});
+        console.log("Created");
+        const accRef = db.collection('Users').doc(email);
+        const doc = await accRef.get();
+        res.status(httpStatus.OK).json(doc.data());
+    } catch(err) {
+        console.log("Could not create new account");
+        next(err);
+    }
+}
+
+
+
+export const deleteAccount = async(req: any, res: any, next: any) => {
+    console.log("Called deleteAccount method");
+    const { accountId } = req.params;
+
+    try {
+        const accRef = db.collection('Users').doc(accountId);
+        const doc = await accRef.get();
+        if (!doc.exists) {
+            console.log("Account does not exist!");
+        }
+        else {
+            db.collection('Users').doc(accountId).delete();
+            console.log("Deleted");
+        }
+        res.status(httpStatus.OK);
+    } catch(err) {
+        console.log("Could not delete account");
+        next(err);
+    }
+}
