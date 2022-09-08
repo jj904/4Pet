cc5d5c63ddb7b373aaca85a01eb7b493a8db730e
jiwu54 <jianbin.wu@sjsu.edu>
2022-09-07 19:58:31 -0700

    update

--- src/4pets-frontend/src/firebase.js
+++ src/4pets-frontend/src/firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import {
-  GoogleAuthProvider,
   getAuth,
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
@@ -31,7 +30,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-const googleAuthProvider = new GoogleAuthProvider();
 
 const registerUser = async (username, email, password) => {
   try {
