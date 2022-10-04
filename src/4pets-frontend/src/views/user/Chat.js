b1bdf258e77b516141a05cd1695585f0e4229e69
jiwu54 <jianbin.wu@sjsu.edu>
2022-10-03 19:09:39 -0700

    add chat function

--- src/4pets-frontend/src/views/user/Chat.js
+++ src/4pets-frontend/src/views/user/Chat.js
@@ -3,7 +3,7 @@ import React, {  useState, useEffect } from "react";
 import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
 import {useAuth} from '../../contexts/AuthContext'
-import { CometChatUI, CometChatUserListWithMessages } from "../../lib/cometchat-pro-react-ui-kit/CometChatWorkspace/src";
+import { CometChatUI } from "../../lib/cometchat-pro-react-ui-kit-3.0.10-1/CometChatWorkspace/src";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 function Chat() {
